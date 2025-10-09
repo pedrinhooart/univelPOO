01fddac13f3fdc8fa6e4f13707e08d628ec4e066
Pedrinho <90850829+pedrinhooart@users.noreply.github.com>
2025-10-09 20:30:15 -0300

    Update conexao.java

--- src/dao/conexao.java
+++ src/dao/conexao.java
@@ -1,5 +1,26 @@
 package dao;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class conexao {
-    
+	private static final String URL = "jdbc:mysql://localhost:3306/becker?useSSL=false&serverTimezone=UTC";
+	private static final String USER = "root"; // seu usuário do MySQL
+	private static final String PASSWORD = "vssql"; // sua senha do MySQL
+
+    public static Connection conectar() {
+        Connection conexao = null;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexao = DriverManager.getConnection(URL, USER, PASSWORD);
+            System.out.println("Conexão estabelecida com sucesso!");
+
+        } catch (ClassNotFoundException e) {
+            System.out.println("Driver JDBC não encontrado: " + e.getMessage());
+        } catch (SQLException e) {
+            System.out.println("Erro ao conectar ao banco de dados: " + e.getMessage());
+        }
+            return conexao;
+    }
 }
